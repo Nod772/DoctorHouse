421e1f6ee2b0e784d5c99e7c071a38d2a9f98e92
Nod772 <54035784+Nod772@users.noreply.github.com>
2020-12-03 19:15:02 +0200

    Add Infrastukture

---
+++ DoctorHouse.Infrastructure/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,14 @@
+﻿using DoctorHouse.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorHouse.Infrastructure.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        string CreateToken(DbUser user);
+    }
+
+    
+}
