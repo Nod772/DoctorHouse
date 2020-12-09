ec7e41858e951bbe179e98819c7fe064de8fca57
Nod772 <54035784+Nod772@users.noreply.github.com>
2020-12-09 18:56:48 +0200

    AddDB

--- DoctorHouse.IdentityServer/Startup.cs
+++ DoctorHouse.IdentityServer/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Formatters.Json;
 
 using Microsoft.IdentityModel.Tokens;
-using IdentityServerHost.Quickstart.UI;
+//using IdentityServerHost.Quickstart.UI;
+using AutoMapper.Configuration;
+using DoctorHouse.DAL;
+using Microsoft.EntityFrameworkCore;
+using DoctorHouse.DAL.Entity;
+using Microsoft.AspNetCore.Identity;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace DoctorHouse.IdentityServer
 {
@@ -19,22 +26,31 @@ namespace DoctorHouse.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; set; }
 
-        public Startup(IWebHostEnvironment environment)
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
             Environment = environment;
+            Configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<EfContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<DbUser, DbRole>()
+                .AddEntityFrameworkStores<EfContext>()
+                .AddDefaultTokenProviders();
             // configure identity server with in-memory stores, keys, clients and resources
             var builder = services.AddIdentityServer()
                   .AddInMemoryIdentityResources(Config.IdentityResources)
                   .AddInMemoryApiScopes(Config.ApiScopes)
                   .AddInMemoryClients(Config.Clients)
                   //.AddInMemoryClients(ConfigGlobal.Clients)
-                  .AddTestUsers(TestUsers.Users);
+                  .AddAspNetIdentity<DbUser>();
 
             builder.AddDeveloperSigningCredential();
         }
@@ -49,7 +65,7 @@ namespace DoctorHouse.IdentityServer
             app.UseStaticFiles();
 
             app.UseIdentityServer();
-            
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
